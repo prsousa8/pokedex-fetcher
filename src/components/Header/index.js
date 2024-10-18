ca7c0668556ec552a310cf86fdf0dc8622329ec1
prsousa8 <prsousa.128@gmail.com>
2024-10-18 09:23:43 -0300

    refactor: ajuste menu tela 450px

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,40 @@
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import './Header.css';
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    // Função para alternar o estado do menu
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
         <header>
             <nav>
-                <Link to='/'><img src='../../assets/logo_pokedex.svg' /></Link>
-                <ul>
-                    <Link to='/pokemons'><li>
-                        <img src='../../assets/pikachu_icon.svg' />
-                        <p className='destaque'>Pokemons</p>
+                <Link to='/'>
+                    <img src='../../assets/logo_pokedex.svg' alt='Logo Pokedex' className="logo" />
+                </Link>
+                <div className="icons-menu" onClick={toggleMenu}>
+                    <img src='../../icons_menu.svg' alt='Menu Icon' />
+                </div>
+                <ul className={`menu-list ${menuOpen ? 'open' : ''}`}>
+                    <li onClick={toggleMenu}>
+                        <Link to='/pokemons' className="menu-item">
+                            <img src='../../assets/pikachu_icon.svg' alt='Icone Pikachu' className="icon" />
+                            <p className='destaque'>Pokemons</p>
+                        </Link>
                     </li>
-                    </Link>
-                    <Link to='/sobre'><li><p className='destaque'>Sobre</p></li></Link>
+                    <li onClick={toggleMenu}>
+                        <Link to='/sobre' className="menu-item">
+                            <p className='destaque'>Sobre</p>
+                        </Link>
+                    </li>
                 </ul>
             </nav>
         </header>
-    )
+    );
 }
 
 export default Header;
